0550bab1812d7e2473efd8246fd7dd9cfd73c422
gbagaev <gbagaev@gmail.com>
2015-06-17 02:34:01 +0300

    fix les_5

--- les_5.rb
+++ les_5.rb
@@ -4,5 +4,5 @@ puts 'What\'s your surname?'
 surname = gets.chomp
 puts 'What\'s your middle name?'
 middle_name = gets.chomp
-full_name_length = name.length + surname.length + middle_name.length
-puts 'In your name ' + full_name.to_s + ' characters.'
+full_name = name + surname + middle_name
+puts 'In your name ' + full_name.length.to_s + ' characters.'
