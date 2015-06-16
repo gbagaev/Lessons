f2f7cbc8189c08afdf71e6e0ebb40272e7626136
gbagaev <gbagaev@gmail.com>
2015-06-16 22:10:06 +0300

    add next files

---
+++ code.rb
@@ -0,0 +1,4 @@
+puts 'what\'s your favorite number?'
+number=gets.chomp
+var=number.to_i+1
+puts 'maybe your favotite number is '+var.to_s+'?'
---
+++ name.rb
@@ -0,0 +1,7 @@
+puts 'what\'s your name?'
+name1=gets.chomp
+puts 'what\'s your surname?'
+name2=gets.chomp
+puts 'what\'s your father\'s name?'
+name3=gets.chomp
+puts 'hi '+name1+name3+name2+'!'
