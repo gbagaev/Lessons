36f9659cb5e04dc13cbcde12f9f3df8f4c0fb46e
gbagaev <gbagaev@gmail.com>
2015-06-16 22:48:30 +0300

    fixed files

--- name.rb
+++ name.rb
@@ -1,7 +1,7 @@
 puts 'what\'s your name?'
-name1=gets.chomp
+name1 = gets.chomp
 puts 'what\'s your surname?'
-name2=gets.chomp
+name2 = gets.chomp
 puts 'what\'s your father\'s name?'
-name3=gets.chomp
-puts 'hi '+name1+name3+name2+'!'
+name3 = gets.chomp
+puts 'hi ' + name1 + name3 + name2 + '!'
