177da29ceab18586a16ea0cb8dd2352cffb17e24
gbagaev <gbagaev@gmail.com>
2015-06-17 02:14:03 +0300

    add les_5

---
+++ les_5.rb
@@ -0,0 +1,8 @@
+puts 'What\'s your name?'
+name = gets.chomp
+puts 'What\'s your surname?'
+surname = gets.chomp
+puts 'What\'s your middle name?'
+middle_name = gets.chomp
+full_name = name.length + surname.length + middle_name.length
+puts 'In your name ' + full_name.to_s + ' characters.'
