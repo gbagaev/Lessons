6a33c5e9afaa175e81316c19ed77748e5ddae57e
gbagaev <gbagaev@gmail.com>
2015-06-18 23:11:17 +0300

    les_5_3.rb

--- les_5_3.rb
+++ les_5_3.rb
@@ -5,8 +5,8 @@ chapter_2 = 'Chapter 2:   Letters'
 chapter_2_page = 'page 72'
 chapter_3 = 'Chapter 3:   Variables'
 chapter_3_page = 'page 118'
-puts tab_of_cont.center lineWidth
+puts tab_of_cont.center line_width
 puts ' '
-puts chapter_1 .ljust(lineWidth/2) + chapter_1_page .rjust(lineWidth/2)
-puts chapter_2 .ljust(lineWidth/2) + chapter_2_page .rjust(lineWidth/2)
-puts chapter_3 .ljust(lineWidth/2) + chapter_3_page .rjust(lineWidth/2)
+puts chapter_1 .ljust(line_width/2) + chapter_1_page .rjust(line_width/2)
+puts chapter_2 .ljust(line_width/2) + chapter_2_page .rjust(line_width/2)
+puts chapter_3 .ljust(line_width/2) + chapter_3_page .rjust(line_width/2)
