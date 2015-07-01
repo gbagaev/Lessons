4424041785ce0f2f8d71d142e84e71a05f2bc32f
gbagaev <gbagaev@gmail.com>
2015-07-01 22:10:10 +0300

    99_bottles.rb

--- 99_bottles.rb
+++ 99_bottles.rb
@@ -1,22 +1,14 @@
 bottles = 99
-while bottles != '94'.to_i
+while bottles > 1
 	puts bottles.to_s + ' bottles of beer on the wall, ' + bottles.to_s + ' bottles of beer.'
-	puts 'Take one down and pass it around, ' + (bottles - 1).to_s + ' of beer on the wall.'
-	puts''
-	puts (bottles - 1).to_s + ' bottles of beer on the wall, ' + (bottles - 1).to_s + ' bottles of beer.'
-	puts 'Take one down and pass it around, ' + (bottles - 2).to_s + ' of beer on the wall.'
-	puts''
-	puts (bottles - 2).to_s + ' bottles of beer on the wall, ' + (bottles - 2).to_s + ' bottles of beer.'
-	puts 'Take one down and pass it around, ' + (bottles - 3).to_s + ' of beer on the wall.'
-	puts''
-	puts (bottles - 3).to_s + ' bottles of beer on the wall, ' + (bottles - 3).to_s + ' bottles of beer.'
-	puts 'Take one down and pass it around, ' + (bottles - 4).to_s + ' of beer on the wall.'
-	puts''
-	puts (bottles - 4).to_s + ' bottles of beer on the wall, ' + (bottles - 4).to_s + ' bottles of beer.'
-	puts 'Take one down and pass it around, ' + (bottles - 5).to_s + ' of beer on the wall.'
-	puts''
-end
+    puts 'Take one down and pass it around, ' + (bottles - 1).to_s + ' of beer on the wall.'
+    puts''
+bottles = bottles - 1
+if bottles == 1
+	puts '1 bottle of beer on the wall, 1 bottles of beer.'
     puts 'Take one down and pass it around, no more bottles of beer on the wall.'
     puts ''
     puts 'No more bottles of beer on the wall, no more bottles of beer.'
     puts 'Go to the store and buy some more, 99 bottles of beer on the wall.'
+end
+end
