0af2c206e9615bfdebb9922b7d5ee6588825f608
gbagaev <gbagaev@gmail.com>
2015-07-01 22:51:13 +0300

    fix_tabs

---
+++ Les.rb
@@ -0,0 +1,19 @@
+puts 'Hi, what\'s your name?'
+name = gets.chomp
+puts 'Hi, ' + name + '.'
+if (name == 'George' or name == 'Katy')
+	puts 'What a lovley name!'
+end
+
+puts 'Hi, what\'s your name?'
+name = gets.chomp
+puts 'Hi, ' + name + '.'
+if name == 'George'
+	puts 'What a lovley name!'
+if name == 'Geo'
+	puts 'What a lovley name!'
+if name == 'Gee'
+	puts 'What a lovley name!'
+end
+end
+end
---
+++ Les_6_2.rb
@@ -0,0 +1,17 @@
+imchris = true
+impurpule = false
+ilikefood = true
+ieatrocks = false
+
+puts (imchris and ilikefood)
+puts (ilikefood and ieatrocks)
+puts (impurpule and ilikefood)
+puts (impurpule and ieatrocks)
+puts
+puts (imchris or ilikefood)
+puts (ilikefood or ieatrocks)
+puts (impurpule or ilikefood)
+puts (impurpule or ieatrocks)
+puts
+puts (not impurpule)
+puts (not imchris)
